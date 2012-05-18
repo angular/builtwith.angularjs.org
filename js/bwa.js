074ef8d2bb3190288f8916fd3dc2803e70f9728d
Brian Ford <btford@umich.edu>
2012-05-18 16:01:42 -0700

    added search by tags

--- js/bwa.js
+++ js/bwa.js
@@ -14,19 +14,57 @@ function BWAController ($scope, $http, $filter) {
           break;
         }
       }
+
+      $scope.tags = [];
+      $scope.activeTags = [];
+
+      // add tags
+      angular.forEach(data.projects, function (project) {
+        angular.forEach(project.tags, function (tag) {
+
+          // ensure tags are unique
+          if ($scope.tags.indexOf(tag) === -1) {
+            $scope.tags.push(tag);
+          }
+        });
+      });
+
+      $scope.tags.sort();
+
       $scope.search();
     }).
     error(function (data, status, headers, config) {
       // TODO: display a nice error message?
+      $scope.error = "Cannot get data from the server";
     });
 
   var num = 2;
   $scope.filteredProjects = [];
   $scope.groupedProjects = [];
 
+  // search helpers
+  var searchMatch = function (haystack, needle) {
+    if (!needle) {
+      return true;
+    }
+    return haystack.toLowerCase().indexOf(needle.toLowerCase()) !== -1;
+  };
+
+  var hasAllTags = function (haystack, needles) {
+    var ret = true;
+    angular.forEach(needles, function (needle) {
+      if (haystack.indexOf(needle) === -1) {
+        ret = false;
+      }
+    });
+    return ret;
+  }
 
   $scope.search = function () {
-    $scope.filteredProjects = $filter('filter')($scope.projects, $scope.query);
+    $scope.filteredProjects = $filter('filter')($scope.projects, function (project) {
+      return (searchMatch(project.desc, $scope.query) || searchMatch(project.name, $scope.query)) &&
+        hasAllTags(project.tags, $scope.activeTags);
+    });
     $scope.group();
   };
 
@@ -46,8 +84,41 @@ function BWAController ($scope, $http, $filter) {
     }
 
     if ($scope.filteredProjects.length % num !== 0) {
-      $scope.groupedProjects[$scope.groupedProjects.length].length = num - ($scope.filteredProjects.length % num);
+      $scope.groupedProjects[$scope.groupedProjects.length - 1].length = num - ($scope.filteredProjects.length % num);
     }
   };
 
+  $scope.addTag = function () {
+    var tagName = this.tag;
+
+    angular.forEach($scope.tags, function (tag, i) {
+      if (i === 0 && tag === tagName) {
+        $scope.activeTags.push($scope.tags.shift());
+      } else if (tag === tagName) {
+        $scope.tags.splice(i, i);
+        $scope.activeTags.push(tag);
+      }
+    });
+
+    $scope.activeTags.sort();
+    $scope.search();
+  };
+
+  // TODO: code duplicated here
+  $scope.removeTag = function () {
+    var tagName = this.tag;
+    console.log(tagName);
+
+    angular.forEach($scope.activeTags, function (tag, i) {
+      if (i === 0 && tag === tagName) {
+        $scope.tags.push($scope.activeTags.shift());
+      } else if (tag === tagName) {
+        $scope.activeTags.splice(i, i);
+        $scope.tags.push(tag);
+      }
+    });
+
+    $scope.tags.sort();
+    $scope.search();
+  };
 };
