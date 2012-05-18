644f4573c4afdb7a80679835e49d101f5398cb68
Brian Ford <btford@umich.edu>
2012-05-18 13:35:32 -0700

    Added basic search

--- js/bwa.js
+++ js/bwa.js
@@ -1,31 +1,53 @@
-function BWAController ($scope, $http) {
 
+function BWAController ($scope, $http, $filter) {
   $http({method: 'GET', url: 'projects.json'}).
-    success(function(data, status, headers, config) {
+    success(function (data, status, headers, config) {
 
-      $scope.numberOfProjects = data.projects.length;
-      $scope.projects = [];
-
-      for (var i = 0; i < data.projects.length; i++) {
-        if (i % 2 === 0) {
-          $scope.projects[Math.floor(i / 2)] = [ data.projects[i] ];
-        } else {
-          $scope.projects[Math.floor(i / 2)].push(data.projects[i]);
-        }
-      }
+      $scope.projects = data.projects;
 
       // find the featured project
       for (var i = $scope.projects.length - 1; i >= 0; i--) {
         if (data.projects[i].name === data.featured) {
           $scope.featured = data.projects[i];
+          // TODO: remove featured project from search?
+          // $scope.projects.slice ...
           break;
         }
       }
-
+      $scope.search();
     }).
-    error(function(data, status, headers, config) {
-      // called asynchronously if an error occurs
-      // or server returns response with status
-      // code outside of the <200, 400) range
+    error(function (data, status, headers, config) {
+      // TODO: display a nice error message?
     });
-}
+
+  var num = 2;
+  $scope.filteredProjects = [];
+  $scope.groupedProjects = [];
+
+
+  $scope.search = function () {
+    $scope.filteredProjects = $filter('filter')($scope.projects, $scope.query);
+    $scope.group();
+  };
+
+  // re-calculate groupedProjects in place 
+  $scope.group = function () {
+
+    // TODO: malicious edge cases (?)
+
+    $scope.groupedProjects.length = Math.ceil($scope.filteredProjects.length / num);
+
+    for (var i = 0; i < $scope.filteredProjects.length; i++) {
+      if (i % num === 0) {
+        $scope.groupedProjects[Math.floor(i / num)] = [ $scope.filteredProjects[i] ];
+      } else {
+        $scope.groupedProjects[Math.floor(i / num)].push($scope.filteredProjects[i]);
+      }
+    }
+
+    if ($scope.filteredProjects.length % num !== 0) {
+      $scope.groupedProjects[$scope.groupedProjects.length].length = num - ($scope.filteredProjects.length % num);
+    }
+  };
+
+};
